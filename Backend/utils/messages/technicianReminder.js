96e2fe28c6183ffd98e3bc5d6bb76ba49b01199f
Deepak M <deepakm.code@gmail.com>
2025-12-16 20:07:05 +0530

    added the template body to technician reminder file

--- Backend/utils/messages/technicianReminder.js
+++ Backend/utils/messages/technicianReminder.js
@@ -1,1 +1,23 @@
 import { sendWhatsappTemplate } from "../sendWhatsapp.js";
+
+export async function sendTechnicianReminder({
+    technicianPhone,
+    technicianName,
+    customerAddress,
+    sheduledDate,
+    sheduledTime
+}){
+    return sendWhatsappTemplate({
+        to:technicianPhone,
+        templateName: "technician_reminder_v1",
+        components: [
+            {
+                type: "body",
+                parameters: [
+                    { type:"text", text: technicianName },
+                    { type:"text", text: customerAddress}
+                ]
+            }
+        ]
+    })
+}
