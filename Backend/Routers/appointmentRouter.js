0d6edf903fef1d677a0c4cef6976338930618e17
Deepak M <deepakm.code@gmail.com>
2025-12-23 22:28:19 +0530

    added the diagnosis completion router

--- Backend/Routers/appointmentRouter.js
+++ Backend/Routers/appointmentRouter.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import authenticate from '../Middleware/authMiddleware.js';
-import { createAppointment } from '../Controllers/appointmentController.js';
+import { completeDiagnosis, createAppointment } from '../Controllers/appointmentController.js';
 
 const router = express.Router();
 
 router.post("/diagnosis",authenticate,createAppointment)
+router.post("/:id/diagnosis-complete",authenticate,completeDiagnosis);
 
 export default router;
