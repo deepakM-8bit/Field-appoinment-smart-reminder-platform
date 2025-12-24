9b03c40eaa44689b9436f71776b0ad795850f46e
Deepak M <deepakm.code@gmail.com>
2025-12-24 18:35:49 +0530

    added the repair approval router

--- Backend/Routers/appointmentRouter.js
+++ Backend/Routers/appointmentRouter.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import authenticate from '../Middleware/authMiddleware.js';
-import { completeDiagnosis, createAppointment } from '../Controllers/appointmentController.js';
+import { approveRepair, completeDiagnosis, createAppointment } from '../Controllers/appointmentController.js';
 
 const router = express.Router();
 
 router.post("/diagnosis",authenticate,createAppointment)
 router.post("/:id/diagnosis-complete",authenticate,completeDiagnosis);
+router.post("/:id/approval-repair",authenticate,approveRepair)
 
 export default router;
