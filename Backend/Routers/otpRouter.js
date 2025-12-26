11015d7fa551f93c83d24ca6f5342de2a580b95a
Deepak M <deepakm.code@gmail.com>
2025-12-26 18:47:03 +0530

    added the request and verify otp router

--- Backend/Routers/otpRouter.js
+++ Backend/Routers/otpRouter.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { requestDiagnosisOtp,verifyDiagnosisOtp } from "../Controllers/otpController.js";
+import { requestOtp,verifyOtp } from "../Controllers/otpController.js";
 import authenticate from '../Middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.post("/appointments/:id/request-otp",authenticate,requestDiagnosisOtp);
-router.post("/appointments/:id/verify-otp",authenticate,verifyDiagnosisOtp);
+router.post("/:id/request-diagnosis-otp", authenticate, requestOtp("start_diagnosis"));
+router.post("/:id/verify-diagnosis-otp", authenticate, verifyOtp("start_diagnosis"));
+
+router.post("/:id/request-repair-otp", authenticate, requestOtp("start_repair"));
+router.post("/:id/verify-repair-otp", authenticate, verifyOtp("start_repair"));
+
+router.post("/:id/request-payment-otp", authenticate, requestOtp("payment"));
+router.post("/:id/verify-payment-otp", authenticate, verifyOtp("payment"));
+
 
 export default router;
