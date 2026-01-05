1517de85008f9a38e4d7447b5610754adeb0b219
Deepak M <deepakm.code@gmail.com>
2026-01-05 21:02:03 +0530

    addedd the technicians today job list router

--- Backend/Routers/appointmentRouter.js
+++ Backend/Routers/appointmentRouter.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import authenticate from '../Middleware/authMiddleware.js';
-import { approveRepair, completeDiagnosis, createAppointment } from '../Controllers/appointmentController.js';
+import { approveRepair, completeDiagnosis, createAppointment, getTodayAppointmentsForTechnician } from '../Controllers/appointmentController.js';
 
 const router = express.Router();
 
 router.post("/diagnosis",authenticate,createAppointment)
 router.post("/:id/diagnosis-complete",authenticate,completeDiagnosis);
 router.post("/:id/repair-approval",authenticate,approveRepair)
+router.get("/technician/today",authenticate,getTodayAppointmentsForTechnician)
 
 export default router;
