26e8874994f4d41767b6815d3f4489f7a9439a57
Deepak M <deepakm.code@gmail.com>
2026-01-20 20:26:20 +0530

    updated env file and db password

--- Backend/db.js
+++ Backend/db.js
@@ -2,15 +2,9 @@ import pkg from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 
-console.log("DATABASE_URL:", process.env.DATABASE_URL); // ✅ TEMP
-console.log(
-  "DB_HOST:",
-  process.env.DATABASE_URL
-    ? new URL(process.env.DATABASE_URL).hostname
-    : "missing",
-);
-
 const { Pool } = pkg;
+
+console.log("DATABASE_URL loaded:", process.env.DATABASE_URL);
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
