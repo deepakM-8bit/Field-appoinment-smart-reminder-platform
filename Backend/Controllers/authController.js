37bb09c406b0557952c7ee94d73527266a825793
Deepak M <deepakm.code@gmail.com>
2026-01-20 22:00:11 +0530

    random updates

--- Backend/Controllers/authController.js
+++ Backend/Controllers/authController.js
@@ -2,129 +2,150 @@ import pool from "../db.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-export const signUpUser = async (req,res) => {
-    const {name,email,password} = req.body;
+export const signUpUser = async (req, res) => {
+  const { name, email, password } = req.body;
 
-    try{
-        const result = await pool.query("SELECT * FROM users WHERE email=$1",[email]);
-        if(result.rows.length>0){
-            return res.status(404).json({message:"user already registered"});
-        }
-        const hashedPassword = await bcrypt.hash(password,10);
+  try {
+    const result = await pool.query("SELECT * FROM users WHERE email=$1", [
+      email,
+    ]);
+    if (result.rows.length > 0) {
+      return res.status(409).json({ message: "user already registered" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-        const insertResult = await pool.query("INSERT INTO users (name,email,password) values ($1,$2,$3) RETURNING id",
-            [name,email,hashedPassword]);
-        res.status(200).json({userId: insertResult.rows[0].id,message:"user registered successfully"});
-    }catch(err){
-        res.status(500).json(err.message);
+    const insertResult = await pool.query(
+      "INSERT INTO users (name,email,password) values ($1,$2,$3) RETURNING id",
+      [name, email, hashedPassword],
+    );
+    res.status(200).json({
+      userId: insertResult.rows[0].id,
+      message: "user registered successfully",
+    });
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const result = await pool.query("SELECT * FROM users WHERE email=$1", [
+      email,
+    ]);
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: "user not found" });
     }
 
-}
+    const user = result.rows[0];
 
-export const loginUser = async(req,res) => {
-    const {email,password} = req.body;
-
-    try{
-        const result = await pool.query("SELECT * FROM users WHERE email=$1",[email]);
-        if(result.rows.length === 0){
-            res.status(404).json({message:"user not found"});
-        }
-
-        const user = result.rows[0];
-        
-        const valid = await bcrypt.compare(password, user.password);
-        if(!valid){
-            return res.status(403).json({message:"incorrect password"});
-        }
-        
-        const token = jwt.sign({id: user.id , email:user.email, name: user.name, role:user.role},
-             process.env.JWT_SECRET);
-        res.json({
-            message: "user found",
-            token: token,
-            role: user.role,
-            user:{
-                id:user.id,
-                name:user.name,
-                email:user.email,
-                role:user.role
-            }
-        });
-        console.log({
-            message:"user logged in",
-            id:user.id,
-            name:user.name,
-            email:user.email,
-            role:user.role})
-    }catch(err){
-        res.status(500).json(err.message);
-    }
-}
-
-export const technicianLogin = async (req,res) => {
-    const {phoneno, password} = req.body;
-
-        if (!password || !phoneno) {
-        return res.status(400).json({ message: "password and phone number required" });
-        }
-
-    try{
-        const result = await pool.query("SELECT * FROM technicians WHERE phone=$1",[phoneno]);
-
-        if(result.rows.length === 0){
-            res.status(404).json({message:"technician not found"});
-        }
-
-        const technician = result.rows[0];
-
-        const valid = await bcrypt.compare(password, technician.password);
-        if(!valid){
-            return res.status(403).json({message: "Invalid password"});
-        }
-
-        const token = jwt.sign({id: technician.id, name: technician.name, role:"technician"}, process.env.JWT_SECRET);
-
-        return res.json({
-            message:"technician login successfully",
-            role:"technician",
-            token,
-            mustChangePassword: technician.must_change_password,
-            technician:{
-                id:technician.id,
-                name:technician.name,
-                category:technician.category,
-            },
-        })
-    }catch(err){
-        console.error("technician login error:",err);
-        return res.status(500).json({message:"server error"});
-    }
-}
-
-export const technicanResetPassword = async (req,res) => {
-    const technicanId = req.user.id;
-    const { newPassword } = req.body;
-
-    if(!newPassword || newPassword.length < 5) {
-        return res.status(400).json({message: "Password must be at least 5 characters"});
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      return res.status(403).json({ message: "incorrect password" });
     }
 
-    try {
-        const hashedPassword = await bcrypt.hash(newPassword, 10);
+    const token = jwt.sign(
+      { id: user.id, email: user.email, name: user.name, role: user.role },
+      process.env.JWT_SECRET,
+    );
+    return res.json({
+      message: "user found",
+      token: token,
+      role: user.role,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+    console.log({
+      message: "user logged in",
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    });
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
 
-        await pool.query(
-            `
+export const technicianLogin = async (req, res) => {
+  const { phoneno, password } = req.body;
+
+  if (!password || !phoneno) {
+    return res
+      .status(400)
+      .json({ message: "password and phone number required" });
+  }
+
+  try {
+    const result = await pool.query(
+      "SELECT * FROM technicians WHERE phone=$1",
+      [phoneno],
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: "technician not found" });
+    }
+
+    const technician = result.rows[0];
+
+    const valid = await bcrypt.compare(password, technician.password);
+    if (!valid) {
+      return res.status(403).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign(
+      { id: technician.id, name: technician.name, role: "technician" },
+      process.env.JWT_SECRET,
+    );
+
+    return res.json({
+      message: "technician login successfully",
+      role: "technician",
+      token,
+      mustChangePassword: technician.must_change_password,
+      technician: {
+        id: technician.id,
+        name: technician.name,
+        category: technician.category,
+      },
+    });
+  } catch (err) {
+    console.error("technician login error:", err);
+    return res.status(500).json({ message: "server error" });
+  }
+};
+
+export const technicanResetPassword = async (req, res) => {
+  const technicanId = req.user.id;
+  const { newPassword } = req.body;
+
+  if (!newPassword || newPassword.length < 5) {
+    return res
+      .status(400)
+      .json({ message: "Password must be at least 5 characters" });
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+    await pool.query(
+      `
             UPDATE technicians
             SET password = $1,
                 must_change_password = false
             WHERE id = $2
             `,
-            [hashedPassword, technicanId]        
-        );
+      [hashedPassword, technicanId],
+    );
 
-        return res.json({message: "Password updated successfully"});
-    } catch (err) {
-        console.error("Reset password error:", err);
-        return res.status(500).json({message: "internal server error"});
-    }
+    return res.json({ message: "Password updated successfully" });
+  } catch (err) {
+    console.error("Reset password error:", err);
+    return res.status(500).json({ message: "internal server error" });
+  }
 };
